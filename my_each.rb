af1a8cb40426d1fd058a80249d8d2bf0f074bf09
Simo Khouili <m.simokhouili@gmail.com>
2020-02-21 23:45:40 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -4,9 +4,10 @@ def my_each(array)
 new_array = []
 counter = 0
 while counter < array.length
-  yield (array[counter])
+  yield (new_array[counter])
   counter += 1
 end
+
 end
 # yield(array[i])
 #     i = i + 1
