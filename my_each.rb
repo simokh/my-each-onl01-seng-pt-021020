2e3e339c6e7745dff2812f37df3bda032c434dbf
Simo Khouili <m.simokhouili@gmail.com>
2020-02-21 23:42:29 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -5,13 +5,9 @@ new_array = []
 counter = 0
 while counter < array.length
   yield (array[counter])
-  counter += 1 
-
-
-
-
-
+  counter += 1
 end
+retur words 
 end
 # yield(array[i])
 #     i = i + 1
