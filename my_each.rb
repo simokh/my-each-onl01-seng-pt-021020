2dde1cef94bf28713e9171e3f8fdc057fc544729
Simo Khouili <m.simokhouili@gmail.com>
2020-02-21 23:21:33 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -4,9 +4,9 @@ def my_each(file)
 new_array = []
 counter = 0
 while new_array < words.length
-  my_each(words) do |item|
-    expect(item).to equal(words[counter])
-    counter+=1
+  # my_each(words) do |item|
+  #   expect(item).to equal(words[counter])
+  #   counter+=1
 
 
 
